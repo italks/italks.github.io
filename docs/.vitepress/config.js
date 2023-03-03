130f45c5905e89c21da14d5b8d85b8342920301d
zhoulizhi <1021639959@qq.com>
2023-03-03 15:15:09 +0800

    提交vitepress配置

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,8 @@
+export default {
+    title: 'VitePress Test',
+    description: 'Just playing around.',
+    //如果要用子目录（https://example.com/subdir/）作为站点，则必须在 docs/.vitepress/config.js 中将 '/subdir/' 设置为 base
+// 示例： 如果您使用 Github（或 GitLab）页面并部署到 user.github.io/repo/，则将 base 设置为 /repo/。
+    base: '/'
+    // srcDir: './docs'
+}
